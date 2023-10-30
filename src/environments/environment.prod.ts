71a0d79165d97fd61bfa3c86f10f24e24e3e4bc5
Mamadou Dian Diallo <86023314+mdianmareyah@users.noreply.github.com>
2023-10-30 12:49:25 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,1 @@
-export const environment = {
-  firebase : {
-    apiKey: "AIzaSyDTOSS6uY5qOlERwTYJOlPzfQOyiKQiZg0",
-    authDomain: "miminet-303fe.firebaseapp.com",
-    projectId: "miminet-303fe",
-    storageBucket: "miminet-303fe.appspot.com",
-    messagingSenderId: "87048015765",
-    appId: "1:87048015765:web:a02c2752d55be0a2aef09c",
-    measurementId: "G-TC1C2B1091"
-  },
-  production: true
-};
+
