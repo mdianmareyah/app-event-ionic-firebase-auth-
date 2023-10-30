9c2b3446ea4c38650db3c0ebca6f19bb14f7f40d
Mamadou Dian Diallo <86023314+mdianmareyah@users.noreply.github.com>
2023-10-30 12:50:11 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase : {
-    apiKey: "AIzaSyDTOSS6uY5qOlERwTYJOlPzfQOyiKQiZg0",
-    authDomain: "miminet-303fe.firebaseapp.com",
-    projectId: "miminet-303fe",
-    storageBucket: "miminet-303fe.appspot.com",
-    messagingSenderId: "87048015765",
-    appId: "1:87048015765:web:a02c2752d55be0a2aef09c",
-    measurementId: "G-TC1C2B1091"
-  },
   production: false
 };
 
